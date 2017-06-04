aafff491bc1f6cde80530b54cacc847b89403f5c
Matias Ribichich <guli4073@gmail.com>
2017-06-04 19:03:49 -0300

    initial migration to react

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Release Management</h2>
+        </div>
+       
+
+      </div>
+    );
+  }
+}
+
+export default App;
